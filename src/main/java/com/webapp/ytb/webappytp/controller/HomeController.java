858e52d3e7f0efd1020a1b81a1bed66338823653
mael <mael>
2023-11-24 16:33:56 +0100

    Merge from Celia

--- src/main/java/com/webapp/ytb/webappytp/controller/HomeController.java
+++ src/main/java/com/webapp/ytb/webappytp/controller/HomeController.java
@@ -1,7 +1,5 @@
 package com.webapp.ytb.webappytp.controller;
 
-import java.time.LocalDate;
-
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -19,22 +17,20 @@ import com.webapp.ytb.webappytp.service.UtilisateurServiceImpl;
 
 @Controller
 public class HomeController {
-
+    
     UtilisateurServiceImpl userServ;
     FicheServiceImpl ficheServ;
-
     public HomeController(UtilisateurServiceImpl userServ, FicheServiceImpl ficheServ) {
         this.userServ = userServ;
         this.ficheServ = ficheServ;
     }
 
-    // Ajouter un apprenti
+    //Ajouter un apprenti
     @GetMapping("/ajout_apprenti")
     public String ajout_apprenti(Model model) {
         model.addAttribute("apprenti", new Utilisateur());
         return "ajout_apprenti";
     }
-
     @PostMapping("/ajouter_apprenti")
     public String ajouter_apprenti(@ModelAttribute Utilisateur user) {
         user.setRole(UserRole.USER);
@@ -42,33 +38,27 @@ public class HomeController {
         return "accueil_admin";
     }
 
-    // Ajouter une fiche
+    //Ajouter une fiche
     @GetMapping("/ajout_fiche")
     public String ajout_fiche(Model model) {
-        FicheIntervention fiche = new FicheIntervention();
-        model.addAttribute("fiche", fiche);
-        model.addAttribute("users", userServ.lire()); // Replace with your actual service method
-        return "fiche_a_completer";
+        model.addAttribute("apprenti", new Utilisateur());
+        return "ajout_fiche";
     }
-
     @PostMapping("/ajouter_fiche")
     public String ajouter_fiche(@ModelAttribute FicheIntervention fiche) {
-        fiche.setDateCreation(LocalDate.now()); // Set the dateCreation to the current date
         ficheServ.creer(fiche);
         return "accueil";
     }
 
-    // Accueils
+    //Accueils
     @GetMapping("/")
     public String home() {
         return "accueil";
     }
-
     @GetMapping("/accueil")
     public String redirectToAccueil() {
         return "accueil";
     }
-
     @GetMapping("/accueil_admin")
     public String admin(Model model, @AuthenticationPrincipal UserDetails userDetails) {
         String role = userDetails.getAuthorities().stream().findFirst().get().getAuthority();
@@ -77,37 +67,38 @@ public class HomeController {
         }
         return "redirect:/accueil";
     }
-
-    // connection
     @GetMapping("/login")
     public String login() {
         return "login";
     }
 
-    // deconnection
-    @GetMapping("/logout")
-    public String logout() {
-        return "logout";
-    }
 
     @GetMapping("/select_fiche")
     public String select_fiche() {
         return "select_fiche";
     }
 
-    // Afficher la fiche no
-    @GetMapping("/fiche/{id}")
-    public String fiche(@PathVariable Long id, Model model) {
-        FicheIntervention ficheIntervention = ficheServ.lire(id);
+    //Afficher la fiche no
+    @GetMapping("/fiche/{numero}")
+    public String fiche(@PathVariable Long numero, Model model) {
+        FicheIntervention ficheIntervention = ficheServ.lire(numero);
         model.addAttribute("ficheIntervention", ficheIntervention);
-        return "fiche_complete";
+        return "index";
+    }
+    //test pour afficher une fiche sans html
+    @GetMapping("/ficheIntervention/{numero}")
+    public String ficheIntervention(@PathVariable Long numero, Model model) {
+        FicheIntervention ficheIntervention = ficheServ.lire(numero);
+        model.addAttribute("ficheIntervention", ficheIntervention);
+        return "ficheIntervention";
     }
 
-    @GetMapping("/suivi_progression")
-    public String suivi_progression() {
-        return "suivi_progression";
+    @GetMapping("/menu")
+    public String menu(Model model, @AuthenticationPrincipal UserDetails userDetails) {
+        String username = userDetails.getUsername();
+        String role = userDetails.getAuthorities().stream().findFirst().get().getAuthority();
+        model.addAttribute("username", username);
+        model.addAttribute("role", role);
+        return "menu";
     }
-
-
-    
 }
