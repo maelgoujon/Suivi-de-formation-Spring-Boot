96aae592022ecc2783acef835ac4e7ee2a8da4d4
mael <mael>
2023-12-15 15:34:52 +0100

    liste fiche et selection icones

--- src/main/java/com/webapp/ytb/webappytp/controller/HomeController.java
+++ src/main/java/com/webapp/ytb/webappytp/controller/HomeController.java
@@ -1,14 +1,12 @@
 package com.webapp.ytb.webappytp.controller;
 
-import java.io.IOException;
-import java.net.http.HttpHeaders;
+import java.io.File;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Base64;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
-import org.hibernate.boot.model.naming.ImplicitJoinColumnNameSource.Nature;
-import org.hibernate.mapping.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -21,16 +19,10 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
-
 import com.webapp.ytb.webappytp.modele.FicheIntervention;
-import com.webapp.ytb.webappytp.modele.UserRole;
 import com.webapp.ytb.webappytp.modele.Utilisateur;
 import com.webapp.ytb.webappytp.modele.ElementsFiche.Maintenance;
 import com.webapp.ytb.webappytp.modele.ElementsFiche.NatureIntervention;
-import com.webapp.ytb.webappytp.repository.FicheRepository;
-import com.webapp.ytb.webappytp.service.FicheService;
 import com.webapp.ytb.webappytp.service.FicheServiceImpl;
 import com.webapp.ytb.webappytp.service.UtilisateurServiceImpl;
 
@@ -110,8 +102,10 @@ public class HomeController {
         return "presentation";
     }
 
-    @GetMapping("/record")
-    public String record() {
+    @GetMapping("/record/{ficheId}")
+    public String record(@PathVariable Long ficheId, Model model) {
+        FicheIntervention ficheIntervention = ficheServ.lire(ficheId);
+        model.addAttribute("ficheIntervention", ficheIntervention);
         return "record";
     }
 
@@ -125,8 +119,10 @@ public class HomeController {
         return "mdp_oublie";
     }
 
-    @GetMapping("/recordaffichage")
-    public String recordaffichage() {
+    @GetMapping("/recordaffichage/{ficheId}")
+    public String recordaffichage(@PathVariable Long ficheId, Model model) {
+        FicheIntervention ficheIntervention = ficheServ.lire(ficheId);
+        model.addAttribute("ficheIntervention", ficheIntervention);
         return "recordaffichage";
     }
 
@@ -149,11 +145,11 @@ public class HomeController {
         }
     }
 
-    @GetMapping("/ficheA/{id}")
+    @GetMapping("/fiche/modifier/{id}")
     public String showFicheDetails(@PathVariable long id, Model model) {
         FicheIntervention ficheIntervention = ficheServ.lire(id);
         model.addAttribute("ficheIntervention", ficheIntervention);
-        return "ficheDetails";
+        return "fiche_modifier";
     }
 
     @PostMapping("/updateFiche/{id}")
@@ -176,10 +172,9 @@ public class HomeController {
             @RequestParam String newMateriau3,
             @RequestParam String newMateriau4,
             @RequestParam String newMateriau5
-            
-            ) {
 
-        // Utilisez newNouvelleIntervention.orElse(false) pour obtenir la valeur du paramètre
+    ) {
+
         boolean nouvelleInterventionValue = newNouvelleIntervention.orElse(false);
 
         FicheIntervention ficheIntervention = ficheServ.lire(id);
@@ -196,7 +191,6 @@ public class HomeController {
         ficheIntervention.setTravauxNonRealises(newTravauxNonRealises);
         ficheIntervention.setNouvelleIntervention(nouvelleInterventionValue);
 
-
         ArrayList<String> materiauxOptions = new ArrayList<>();
         materiauxOptions = (ArrayList<String>) ficheIntervention.getMateriauxOptions();
         materiauxOptions.set(0, newMateriau0);
@@ -205,11 +199,47 @@ public class HomeController {
         materiauxOptions.set(3, newMateriau3);
         materiauxOptions.set(4, newMateriau4);
         materiauxOptions.set(5, newMateriau5);
-        
+
         ficheIntervention.setMateriauxOptions(materiauxOptions);
-        
+
         ficheServ.modifier(id, ficheIntervention);
         return "redirect:/fiche/" + id;
+    }
+
+    @GetMapping("/test")
+    public String showGallery(Model model) {
+        ArrayList<String> listeImages = new ArrayList<>();
+        // repertoire des images
+        String directoryPath = "src/main/resources/static/images/materiel/general/";
+        File directory = new File(directoryPath);
+        // tous les fichiers dans le répertoire qui se terminent par ".png"
+        File[] pngFiles = directory.listFiles((dir, name) -> {
+            String lowerCaseName = name.toLowerCase();
+            return lowerCaseName.endsWith(".jpg") || lowerCaseName.endsWith(".jpeg") || lowerCaseName.endsWith(".png") || lowerCaseName.endsWith(".gif") || lowerCaseName.endsWith(".bmp");
+        });
+        
+        if (pngFiles != null) {
+            // ajout des noms des fichiers .png avec le préfixe "/images/" à la liste
+            Arrays.stream(pngFiles).map(file -> "/images/materiel/general/" + file.getName()).forEach(listeImages::add);
+        } else {
+            listeImages.add("/images/clock.png"); // si la liste est vide
+        }
+
+        System.out.println(listeImages);
+
+        model.addAttribute("imageList", listeImages);
+
+        return "testListe";
+    }
+
+    @GetMapping("/fiche/liste_fiche")
+    public String showCreateInterventionForm(Model model) {
+        List<Utilisateur> users = userServ.lire();
+        List<FicheIntervention> fiches = ficheServ.lireTout(); // Ajout de la liste des fiches
+        model.addAttribute("fiche", new FicheIntervention());
+        model.addAttribute("users", users);
+        model.addAttribute("fiches", fiches); // Ajout de la liste des fiches
+        return "liste_fiche";
     }
 
 }
--- src/main/java/com/webapp/ytb/webappytp/controller/UtilisateurController.java
+++ src/main/java/com/webapp/ytb/webappytp/controller/UtilisateurController.java
@@ -6,12 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.webapp.ytb.webappytp.modele.Utilisateur;
 import com.webapp.ytb.webappytp.service.UtilisateurService;
 
@@ -21,7 +17,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 
 @Controller
 @RequestMapping("/utilisateur")
