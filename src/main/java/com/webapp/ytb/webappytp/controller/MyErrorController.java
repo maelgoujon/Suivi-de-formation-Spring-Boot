195be65756304a65cb118018ea7dddc437506c15
mael <mael>
2023-12-25 22:29:46 +0100

    Materiauxamenagement et fiche intervention2

---
+++ src/main/java/com/webapp/ytb/webappytp/controller/MyErrorController.java
@@ -0,0 +1,36 @@
+package com.webapp.ytb.webappytp.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class MyErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        // Récupérer des détails sur l'erreur à partir de la requête
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            if (statusCode == HttpStatus.FORBIDDEN.value()) {
+                return "error403"; // Page d'erreur personnalisée pour le statut 403
+            } else if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error404"; // Page d'erreur personnalisée pour le statut 404
+            }
+        }
+
+        return "error"; // Page d'erreur générale
+    }
+
+    public String getErrorPath() {
+        return "/error";
+    }
+}
---
+++ src/main/java/com/webapp/ytb/webappytp/repository/MateriauxRepository.java
@@ -0,0 +1,11 @@
+package com.webapp.ytb.webappytp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.webapp.ytb.webappytp.modele.ElementsFiche.MateriauxAmenagement;
+
+@Repository
+public interface MateriauxRepository extends JpaRepository<MateriauxAmenagement, Long> {
+        // Ajoutez des méthodes personnalisées si nécessaire
+}
