805dec5db371bfe4732f1b7f813cac06cb525bf4
Mael G <mael.goujon@etu.iut-tlse3.fr>
2024-01-10 14:26:54 +0100

    Creation automoatique des données

--- src/main/java/com/webapp/ytb/webappytp/repository/FicheRepository.java
+++ src/main/java/com/webapp/ytb/webappytp/repository/FicheRepository.java
@@ -9,4 +9,5 @@ import com.webapp.ytb.webappytp.modele.FicheIntervention;
 
 public interface FicheRepository extends JpaRepository<FicheIntervention, Long> {
     List<FicheIntervention> findByUtilisateurId(Long userId);
+    FicheIntervention findByTravauxRealisesAndUtilisateurPrenom(String travauxRealises, String prenom);
 }
