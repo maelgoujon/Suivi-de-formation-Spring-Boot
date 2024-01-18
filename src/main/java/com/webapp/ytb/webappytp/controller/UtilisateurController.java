f05091f1707b1f8b6c4403cd1abd99a9933387aa
Mael G <mael.goujon@etu.iut-tlse3.fr>
2024-01-18 09:23:12 +0100

    Message succes modif

--- src/main/java/com/webapp/ytb/webappytp/controller/UtilisateurController.java
+++ src/main/java/com/webapp/ytb/webappytp/controller/UtilisateurController.java
@@ -1,6 +1,5 @@
 package com.webapp.ytb.webappytp.controller;
 
-
 import java.util.List;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,11 +42,12 @@ public class UtilisateurController {
         // Récupérez les rôles définis dans l'enum UserRole
         UserRole[] roles = UserRole.values();
 
-        // Récupérez la liste des formations depuis la base de données en utilisant le service
+        // Récupérez la liste des formations depuis la base de données en utilisant le
+        // service
         List<Formation> allFormations = formationService.lire();
 
         model.addAttribute("roles", roles);
-        model.addAttribute("allFormations", allFormations); 
+        model.addAttribute("allFormations", allFormations);
         model.addAttribute("utilisateur", new Utilisateur());
         return "nouvelUtilisateur";
     }
@@ -80,9 +80,6 @@ public class UtilisateurController {
         return utilisateurs;
     }
 
-
-    
-
     @GetMapping("/details/{id}")
     public String afficherDetailsUtilisateur(@PathVariable Long id, Model model) {
         Utilisateur utilisateur = utilisateurService.findById(id);
@@ -97,17 +94,16 @@ public class UtilisateurController {
         return "accueil";
     }
 
-    
-
     @GetMapping("/modifier/{id}")
     public String afficherFormulaireModificationUtilisateur(@PathVariable Long id, Model model) {
         Utilisateur utilisateur = utilisateurService.findById(id);
-        
+
         model.addAttribute("utilisateur", utilisateur);
+        UserRole[] roles = UserRole.values();
+        model.addAttribute("roles", roles);
         return "modif";
     }
 
-    
     @PostMapping("/modifier/{id}")
     public String modifierUtilisateur(@PathVariable Long id, @ModelAttribute Utilisateur utilisateur,
             RedirectAttributes redirectAttributes, Model model) {
@@ -115,20 +111,23 @@ public class UtilisateurController {
         // Récupérez les rôles définis dans l'enum UserRole
         UserRole[] roles = UserRole.values();
         model.addAttribute("roles", roles);
+
         List<Formation> allFormations = formationService.lire();
         model.addAttribute("allFormations", allFormations);
         redirectAttributes.addFlashAttribute("success", true);
-        // Ajoutez également les modèles aux attributs flash pour les rendre disponibles après la redirection
+        // Ajoutez également les modèles aux attributs flash pour les rendre disponibles
+        // après la redirection
         redirectAttributes.addFlashAttribute("roles", roles);
         redirectAttributes.addFlashAttribute("allFormations", allFormations);
-        return "redirect:/utilisateur/modifier/" + id;
+        return "redirect:/modif/" + id;
     }
 
     @PostMapping("/modifmdp/{id}")
     public String modifierMotDePasse(@PathVariable Long id, @ModelAttribute Utilisateur utilisateur,
             RedirectAttributes redirectAttributes) {
         try {
-            // Vous pouvez maintenant utiliser utilisateur.getMdp() pour obtenir le mot de passe sélectionné
+            // Vous pouvez maintenant utiliser utilisateur.getMdp() pour obtenir le mot de
+            // passe sélectionné
             utilisateurService.modifierMotDePasse(id, utilisateur.getMdp());
             redirectAttributes.addFlashAttribute("success", true);
             return "redirect:/utilisateur/modifmdp/" + id + "?success=true";
@@ -139,17 +138,12 @@ public class UtilisateurController {
         }
     }
 
-
-
     @GetMapping("/modifmdp/{id}")
     public String afficherFormulaireModificationMotDePasse(@PathVariable Long id, Model model) {
         Utilisateur utilisateur = utilisateurService.findById(id);
         model.addAttribute("utilisateur", utilisateur);
         return "mdpmodif";
     }
-
-    
-
 
     // ToDo: Fix the delete method
     @DeleteMapping("/supprimer/{id}")
@@ -158,6 +152,7 @@ public class UtilisateurController {
         model.addAttribute("success", true);
         return "redirect:/utilisateur/nouveau";
     }
+
     @GetMapping("/tousLesUtilisateurs")
     public String afficherTousLesUtilisateurs(Model model) {
         List<Utilisateur> utilisateurs = utilisateurService.lire();
@@ -171,24 +166,24 @@ public class UtilisateurController {
         if (utilisateur == null) {
             throw new RuntimeException("User not found with ID: " + userId);
         }
-    
+
         response.setContentType("application/vnd.ms-excel");
         String headerKey = "Content-Disposition";
         String userNameForFile = utilisateur.getNom().replace(" ", "_");
         String userFirstNameForFile = utilisateur.getPrenom().replace(" ", "_");
         String headerValue = "attachment; filename=archive_apprenti_" + userNameForFile + userFirstNameForFile + ".xls";
         response.setHeader(headerKey, headerValue);
-    
+
         utilisateurService.generatedExcelForUser(userId, response);
-    
+
         response.flushBuffer();
-    
+
         // Delete fiches d'intervention for the user
         List<FicheIntervention> fiches = ficheService.getFichesByUserId(userId);
         for (FicheIntervention fiche : fiches) {
             ficheService.supprimer(fiche.getId());
         }
-    
+
         // Delete the user
         utilisateurService.supprimer(userId);
     }
