3480782a368f60e5e7e585ab8e8f4299f21fa7ee
Mael <mael.goujon@etu.iut-tlse3.fr>
2024-02-29 16:18:18 +0100

    installation d'OpenJDK 17

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@
 function sudo_install {
     # check if sudo command is installed
     if [ ! -x /usr/bin/sudo ]; then
-        echo "sudo command is not installed, installing it now..."
+        echo "Commande sudo n'est pas installée, installation en cours..."
         apt-get update
         apt-get install sudo -y
         # check if sudo command is installed
         if [ $? -eq 0 ]; then
-            echo "### sudo command installed successfully!"
+            echo "### Commande sudo installée avec succès!"
         else
-            echo "### sudo command installation failed!"
+            echo "### Echec installation commande sudo"
         fi
     fi
 }
@@ -18,24 +18,24 @@ function sudo_install {
 function postgres_install {
     # check if PostgreSQL is already installed
     if [ -x /usr/bin/psql ]; then
-        echo "### PostgreSQL is already installed!"
+        echo "### PostgreSQL est déjà installé!"
     else
-        echo "PostgreSQL is not installed, installing it now..."
+        echo "### PostgreSQL n'est pas installé, installation en cours..."
         sudo apt-get update
         sudo apt-get install postgresql -y
         # check if PostgreSQL is installed
         if [ $? -eq 0 ]; then
-            echo "### PostgreSQL installed successfully!"
+            echo "### PostgreSQL installé avec succès"
         else
-            echo "### PostgreSQL installation failed!"
+            echo "### Echec installation PostgreSQL!"
         fi
     fi
 }
 
 function configure_postgresql {
     # change the password of the default user
-    echo "### Changing the password of the default user..."
-    echo "### Enter the new password for the default user"
+    echo "### Changement du mot de passe de l'utilisateur par défaut (postgres)"
+    echo "### Entrez le nouveau mot de passe:"
     read -s -r password
     escaped_password=$(printf '%q' "$password")
     su - postgres -c "psql -c \"ALTER USER postgres WITH PASSWORD '$escaped_password';\""
@@ -43,33 +43,32 @@ function configure_postgresql {
     # check if the database already exists
     sudo -u postgres psql -lqt | cut -d \| -f 1 | grep -qw apeajdb
     if [ $? -eq 0 ]; then
-        echo "### Database already exists, resetting it now..."
+        echo "### La base de données existe déjà, réinitialisation en cours..."
         sudo -u postgres psql -c "DROP DATABASE apeajdb;"
         create_database
     else
-        echo "### Database does not exist, creating it now..."
+        echo "### La base de données n'existe pas, création en cours..."
         create_database
     fi
-    
     
     # Restart PostgreSQL
     sudo service postgresql restart
     
     # echo the result of the status of PostgreSQL
     if [ $? -eq 0 ]; then
-        echo "### PostgreSQL installed and configured successfully!"
+        echo "### Installation de PostgreSQL réussie!"
     else
-        echo "### PostgreSQL installation and configuration failed!"
+        echo "### Echec de l'installation de PostgreSQL!"
     fi
     
     # if the database exists, reset it else if the database does not exist, create it
     sudo -u postgres psql -lqt | cut -d \| -f 1 | grep -qw apeajdb
     if [ $? -eq 0 ]; then
-        echo "### Database already exists, resetting it now..."
+        echo "### La base de données existe déjà, réinitialisation en cours..."
         sudo -u postgres psql -c "DROP DATABASE apeajdb;"
         create_database
     else
-        echo "### Database does not exist, creating it now..."
+        echo "### La base de données n'existe pas, création en cours..."
         create_database
     fi
 }
@@ -79,23 +78,42 @@ function create_database {
     sudo -u postgres createdb apeajdb
 }
 
+function openjdk17_install {
+    # Install OpenJDK 17
+    sudo apt-get update
+    sudo apt-get install openjdk-17-jdk -y
+    # check if openjdk-17 is installed
+    if [ $? -eq 0 ]; then
+        echo "### Installation OpenJDK 17 réussie!"
+    else
+        echo "### Echec de l'installation OpenJDK 17!"
+    fi
+    
+}
+
 echo "#################################################################"
 echo "##################### Installation démarrée #####################"
 echo "#################################################################"
 
-sudo_install
+#sudo_install
 
 # Install PostgreSQL
 echo "#################################################################"
-echo "##################### Installing PostgreSQL #####################"
+echo "#################### Installation PostgreSQL ####################"
 echo "#################################################################"
 
-postgres_install
+#postgres_install
 
 # Configure PostgreSQL
 echo "#################################################################"
-echo "##################### Configure PostgreSQL ######################"
+echo "#################### Configuration PostgreSQL ###################"
 echo "#################################################################"
 
-configure_postgresql
+#configure_postgresql
 
+# Install openjdk-17
+echo "#################################################################"
+echo "#################### Installation OpenJDK 17 ####################"
+echo "#################################################################"
+
+openjdk17_install
