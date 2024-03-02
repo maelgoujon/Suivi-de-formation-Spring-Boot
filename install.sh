dd8568084c64f6e0691c62d359b756e2fb83f0e1
Mael <mael.goujon@etu.iut-tlse3.fr>
2024-03-02 14:45:18 +0100

    utilisation du fichier .env pour stocker le mdp de la BD

--- install.sh
+++ install.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+
+
 function sudo_install {
-    # check if sudo command is installed
-    if [ ! -x /usr/bin/sudo ]; then
-        echo "Commande sudo n'est pas installée, installation en cours..."
-        apt-get update
+    # check if sudo is already installed
+    if [ -x /usr/bin/sudo ]; then
+        echo "### sudo est déjà installé!"
+    else
+        echo "### sudo n'est pas installé, installation en cours..."
+        
         apt-get install sudo -y
-        # check if sudo command is installed
+        # check if sudo is installed
         if [ $? -eq 0 ]; then
-            echo "### Commande sudo installée avec succès!"
+            echo "### sudo installé avec succès"
         else
-            echo "### Echec installation commande sudo"
+            echo "### Echec installation sudo!"
         fi
     fi
 }
@@ -21,8 +25,8 @@ function postgres_install {
         echo "### PostgreSQL est déjà installé!"
     else
         echo "### PostgreSQL n'est pas installé, installation en cours..."
-        sudo apt-get update
-        sudo apt-get install postgresql -y
+        
+        apt-get install postgresql -y
         # check if PostgreSQL is installed
         if [ $? -eq 0 ]; then
             echo "### PostgreSQL installé avec succès"
@@ -39,6 +43,38 @@ function configure_postgresql {
     read -s -r password
     escaped_password=$(printf '%q' "$password")
     su - postgres -c "psql -c \"ALTER USER postgres WITH PASSWORD '$escaped_password';\""
+    if [ $? -eq 0 ]; then
+        echo "### Mot de passe de l'utilisateur par défaut modifié avec succès!"
+    else
+        echo "### Echec de la modification du mot de passe de l'utilisateur par défaut!"
+    fi
+    # create a .env file for the database password
+    if [ -f /home/etu/.env ]; then
+        echo "### Le fichier .env existe déjà, reinitialisation en cours..."
+        rm /home/etu/.env
+    else
+        echo "### Le fichier .env n'existe pas, création en cours..."
+    fi
+    echo "DATABASE_PASSWORD=$escaped_password" > /home/etu/.env
+    
+    # add postgres user to etu group to allow access to the etu directory
+    #check if the group already exists
+    grep -q etugroup /etc/group
+    if [ $? -eq 0 ]; then
+        echo "### Le groupe etugroup existe déjà, reinitialisation en cours..."
+        groupdel etugroup
+    else
+        echo "### Le groupe etugroup n'existe pas, création en cours..."
+    fi
+    groupadd etugroup
+    usermod -a -G etugroup etu
+    usermod -a -G etugroup postgres
+    chgrp etugroup /home/etu/
+    if [ $? -eq 0 ]; then
+        echo "### Groupe etugroup créé avec succès!"
+    else
+        echo "### Echec de la création du groupe etugroup!"
+    fi
     
     # check if the database already exists
     sudo -u postgres psql -lqt | cut -d \| -f 1 | grep -qw apeajdb
@@ -52,7 +88,7 @@ function configure_postgresql {
     fi
     
     # Restart PostgreSQL
-    sudo service postgresql restart
+    service postgresql restart
     
     # echo the result of the status of PostgreSQL
     if [ $? -eq 0 ]; then
@@ -61,32 +97,23 @@ function configure_postgresql {
         echo "### Echec de l'installation de PostgreSQL!"
     fi
     
-    # if the database exists, reset it else if the database does not exist, create it
-    sudo -u postgres psql -lqt | cut -d \| -f 1 | grep -qw apeajdb
-    if [ $? -eq 0 ]; then
-        echo "### La base de données existe déjà, réinitialisation en cours..."
-        sudo -u postgres psql -c "DROP DATABASE apeajdb;"
-        create_database
-    else
-        echo "### La base de données n'existe pas, création en cours..."
-        create_database
-        if [ $? -eq 0 ]; then
-            echo "### Base de données créée avec succès!"
-        else
-            echo "### Echec de la création de la base de données!"
-        fi
-    fi
 }
 
 function create_database {
     # Create database
     sudo -u postgres createdb apeajdb
+    # check if the database is created
+    if [ $? -eq 0 ]; then
+        echo "### Base de données créée avec succès!"
+    else
+        echo "### Echec de la création de la base de données!"
+    fi
 }
 
 function openjdk17_install {
     # Install OpenJDK 17
-    sudo apt-get update
-    sudo apt-get install openjdk-17-jdk -y
+    
+    apt-get install openjdk-17-jdk -y
     # check if openjdk-17 is installed
     if [ $? -eq 0 ]; then
         echo "### Installation OpenJDK 17 réussie!"
@@ -96,16 +123,89 @@ function openjdk17_install {
     
 }
 
-function start_app {
-    java -jar webappytp-0.0.1-SNAPSHOT.war
+function wifi_hotspot_install {
+    # check if network-manager is already installed
+    if [ -x /usr/sbin/NetworkManager ]; then
+        echo "### Network Manager est déjà installé!"
+    else
+        echo "### Network Manager n'est pas installé, installation en cours..."
+        
+        apt-get install network-manager -y
+        # check if network-manager is installed
+        if [ $? -eq 0 ]; then
+            echo "### Network Manager installé avec succès"
+        else
+            echo "### Echec installation Network Manager!"
+        fi
+    fi
 }
+
+function wifi_hotspot_configure {
+    # ask user for the name of the hotspot and the password
+    echo "### Entrez le nom du hotspot wifi (SSID):"
+    read ssid
+    echo "### Entrez le mot de passe du hotspot wifi:"
+    read -s -r wifi_password
+    echo "### Configuration du hotspot en cours..."
+    nmcli d wifi hotspot ifname wlp3s0 ssid $ssid password $wifi_password
+    # check if the hotspot is configured
+    if [ $? -eq 0 ]; then
+        echo "### Hotspot configuré avec succès!"
+    else
+        echo "### Echec de la configuration du hotspot!"
+    fi
+}
+
+function configure_service {
+    # Configure service to start at boot
+    if [ -f /etc/systemd/system/apeaj.service ]; then
+        echo "### Service déjà configuré!"
+        rm /etc/systemd/system/apeaj.service
+    else
+        echo "### Configuration du service en cours..."
+    fi
+    cp apeaj.service /etc/systemd/system/apeaj.service
+    systemctl enable apeaj.service
+    systemctl start apeaj.service
+    # check if the service is running
+    if [ $? -eq 0 ]; then
+        echo "### Service configuré avec succès!"
+    else
+        echo "### Echec de la configuration du service!"
+    fi
+}
+
+# Check if root user is logged in
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Vous devez etre root (sudo ./install.sh) pour exécuter ce script."
+    exit 1
+fi
 
 echo "#################################################################"
 echo "##################### Installation démarrée #####################"
 echo "#################################################################"
 
+# Update the system
+echo "### Mise à jour du système en cours..."
+apt-get update
+apt-get upgrade -y
+if [ $? -eq 0 ]; then
+    echo "### Mise à jour du système réussie!"
+else
+    echo "### Echec de la mise à jour du système!"
+fi
+
+# Install sudo
+echo "#################################################################"
+echo "#################### Installation de sudo #######################"
+echo "#################################################################"
+
 sudo_install
 
+# check if service apeaj is running and stop it
+if [ -f /etc/systemd/system/apeaj.service ]; then
+    service apeaj stop
+fi
 # Install PostgreSQL
 echo "#################################################################"
 echo "#################### Installation PostgreSQL ####################"
@@ -126,3 +226,46 @@ echo "#################### Installation OpenJDK 17 ####################"
 echo "#################################################################"
 
 openjdk17_install
+
+# Installation Network Manager
+echo "#################################################################"
+echo "#################### Installation Network Manager ###############"
+echo "#################################################################"
+
+wifi_hotspot_install
+
+# Configuration du hotspot wifi
+echo "#################################################################"
+echo "#################### Configuration du hotspot wifi ###############"
+echo "#################################################################"
+
+wifi_hotspot_configure
+
+# Setup images directory
+echo "#################################################################"
+echo "############# Repertoire des images préenregistrées #############"
+echo "#################################################################"
+
+if [ -d /home/etu/images ]; then
+    echo "### Le répertoire des images existe déjà!"
+    rm -r /home/etu/images
+fi
+echo "### Création du répertoire des images..."
+# Extract in background
+tar -xJvf images.tar.xz -C /home/etu/ &
+# set the group of the directory to etugroup
+chgrp etugroup /home/etu/images
+#set the permissions of the directory to 775 recursively
+chmod -R 775 /home/etu/images
+if [ $? -eq 0 ]; then
+    echo "### Répertoire des images créé avec succès!"
+else
+    echo "### Echec de la création du répertoire des images!"
+fi
+
+# Configure service to start at boot
+echo "#################################################################"
+echo "#################### Configuration du service ###################"
+echo "#################################################################"
+
+configure_service
